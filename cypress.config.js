33ba055d02f8859a28da5b26e0f8c90ff6579254
Doston <algorberun@gmail.com>
2022-07-19 01:09:30 +0500

    werewr

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  reporter: "junit",
-  reporterOptions: {
-    mochaFile: "cypress/results/output.xml",
-  },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require("./cypress/plugins/index.js")(on, config);
+      // implement node event listeners here
     },
-    baseUrl: "http://localhost:8080",
   },
 });
